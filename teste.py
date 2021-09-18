8b4531809c9c32136cf9724340814a20e318b078
terrotar <pedro_guedes93@hotmail.com>
2021-09-18 01:41:14 -0300

    PokeAPI connected with requests

--- teste.py
+++ teste.py
@@ -1,6 +1,14 @@
 
+import requests
 
-a = 12
-b = 6
 
-print(a+b)
+class Pokemon:
+
+    def all(self):
+        url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+        response = requests.get(url)
+        return response.json()
+
+
+poke = Pokemon()
+print(poke.all())
