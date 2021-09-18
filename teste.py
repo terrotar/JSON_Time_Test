c5730a6b88ef3ee70b166377594fe57b864ed0ee
terrotar <pedro_guedes93@hotmail.com>
2021-09-18 01:51:47 -0300

    funtion get_pokemon implemented also as well it's error input treatment typed by user

--- teste.py
+++ teste.py
@@ -9,6 +9,15 @@ class Pokemon:
         response = requests.get(url)
         return response.json()
 
+    def get_pokemon(self, pokemon):
+        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+        response = requests.get(url)
+        if ("Not Found" in response.text):
+            return f"Pokémon {pokemon} not found."
+        return response.json()
+
 
 poke = Pokemon()
-print(poke.all())
+# print(poke.all())
+# print(poke.get_pokemon("charmandeR"))
+print(poke.get_pokemon("charmander"))
