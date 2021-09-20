6f80bcefe2112ccaa8de32bad2580e4fa69fb607
terrotar <pedro_guedes93@hotmail.com>
2021-09-20 19:47:29 -0300

    Add a new test

--- app/test_poke_api.py
+++ app/test_poke_api.py
@@ -9,3 +9,10 @@ def test_capital_letter_pokemon():
     poke = poke_api.Pokemon()
     poke = poke.get_pokemon("charmandeR")
     assert "not found" in poke
+
+
+# Test get prokemon with invalid input
+def test_invalid_input__pokemon():
+    poke = poke_api.Pokemon()
+    poke = poke.get_pokemon("teste02")
+    assert "not found" in poke
